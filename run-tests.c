d3ee5c3093288b8f53b78ab36d78e6e4a970a33e
Matthew Millican <millimat@stanford.edu>
2016-05-12 20:25:38 -0700

    Shipping to Alex

--- run-tests.c
+++ run-tests.c
@@ -4,16 +4,23 @@
 #include "softheap.h"
 
 
-#define N_ELEMENTS (1 << 15)
+#define N_ELEMENTS (1 << 18)
 #define SORTED_EPSILON ((double)1 / N_ELEMENTS)
 #define EPSILON 0.1
 #define MAGIC_PRIME_ONE 1399
 #define MAGIC_PRIME_TWO 1093
 
+/* Returns negative number if one < two, positive if one > two,
+ * 0 if one == two. */
 static inline int intcmp(const void *one, const void *two) {
   return *(int *)one - *(int *)two;
 }
 
+/* Report two metrics of soft heap error rate:
+ * 1. How many elements came out with ckeys different from their real keys?
+ * 2. How many elements are not in the same position they would be in a 
+ *    sorted output list?
+ */
 static void report_corruptions(int ckey_corruptions, int pos_corruptions, int nelems) {
   printf("\nTotal number of ckey corruptions: %d\nFraction corrupted: %4.3f\n", ckey_corruptions,
          (double)ckey_corruptions/nelems);
@@ -22,8 +29,10 @@ static void report_corruptions(int ckey_corruptions, int pos_corruptions, int ne
   printf("\n");
 }
 
-static void straight_test(int elems[], int results[][2]) {
-  printf("----------STRAIGHT TEST----------\n");
+
+/* Simple usage pattern: Insert increasing sequence of integers and then extract all. */
+static void forwards_test(int elems[], int results[][2]) {
+  printf("----------FORWARDS TEST----------\n");
   printf("Inserting integers 0 to %d into a soft heap...\n", N_ELEMENTS - 1);
   softheap *P = makeheap_empty(SORTED_EPSILON);
   for(int i = 0; i < N_ELEMENTS; i++) {
@@ -43,6 +52,7 @@ static void straight_test(int elems[], int results[][2]) {
   free(P);
 }
 
+/* Simple usage pattern: Insert decreasing sequence of integers and then extract all. */
 static void backwards_test(int elems[], int results[][2]) {
   printf("----------BACKWARDS TEST----------\n");
   printf("Inserting integers 0 to %d into a soft heap in reverse order...\n", N_ELEMENTS - 1);
@@ -64,6 +74,8 @@ static void backwards_test(int elems[], int results[][2]) {
   free(P);
 }
 
+/* Use multiplication and modulos by primes to feed a random-looking sequence into the
+ * soft heap, then extract all. */
 static void coprime_test(int elems[], int results[][2]) {
   printf("----------COPRIME TEST----------\n");
   printf("Inserting integers (%d * i) mod %d into a soft heap for i = 0 to %d...\n",
@@ -92,6 +104,7 @@ static void coprime_test(int elems[], int results[][2]) {
   free(P);
 }
 
+/* Insert a bunch of random numbers into the heap, then extract them all */
 static void random_test(int elems[], int results[][2]) {
   srand(time(NULL));
   printf("----------RANDOM TEST----------\n");
@@ -119,6 +132,7 @@ static void random_test(int elems[], int results[][2]) {
   free(P);
 }
 
+/* Make sure heap destruction isn't broken */
 static void cleanup_test() {
   printf("----------CLEANUP TEST-----------\n");
   printf("Testing robustness of destroy_heap by creating and destroying "
@@ -128,7 +142,7 @@ static void cleanup_test() {
 
   for(int i = 0; i < 100; i++) {
     int size = N_ELEMENTS / 100 * i;
-    softheap *P = makeheap_empty(SORTED_EPSILON); // make them super branchy
+    softheap *P = makeheap_empty(SORTED_EPSILON); // make heaps super branchy
     
     for(int j = 0; j < size; j++) insert(P, rand());
 
@@ -147,7 +161,7 @@ int main() {
   int sorted[N_ELEMENTS];
   int results[N_ELEMENTS][2];
   
-  straight_test(sorted, results);
+  forwards_test(sorted, results);
   backwards_test(sorted, results);
   coprime_test(sorted, results);
   random_test(sorted, results);
